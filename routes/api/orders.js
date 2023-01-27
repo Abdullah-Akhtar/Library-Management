45f1c47c6603a371bd03cf2daf7b602334ce39b0
Abdullah-Akhtar <mak.abdullah07@gmail.com>
2023-01-27 19:59:35 +0500

    order

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -2,17 +2,19 @@ const router = require("express").Router();
 const auth = require("../../middleware/auth");
 const orders = require("../../models/Order");
 const Books = require("../../models/Books");
- 
-router.get("/addOrder", auth.isLogin, (req, res) => {
-  Books.findOne({title:req.body.title},(err , data)=>{
-    if(!err && data){
-        const newOrder = new orders();
-        newOrder.user = req.user.id;
-    }
-    res.send("No Books Available")
-  })
-    
-  
+
+router.post("/addOrder", auth.isLogin, (req, res) => {
+      const newOrder = new orders();
+      newOrder.user = req.user.id;
+      newOrder.books = req.body.titles;
+      newOrder
+        .save()
+        .then((result) => {
+          res.status(200).send("Order Complete");
+        })
+        .catch((err) => {
+          res.status(444).send("Something went wrong");
+        });
 });
 
 module.exports = router;
